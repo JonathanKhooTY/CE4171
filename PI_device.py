d444d0b55442e598785559963f38e162931c10fc
JonathanKhooTY <jonathan.khooty@gmail.com>
2022-04-07 12:00:46 +0800

    updated PI_device code

--- PI_device.py
+++ PI_device.py
@@ -1,7 +1,19 @@
 import paho.mqtt.publish as publish
+from picamera import PiCamera
+from time import sleep
+
+
 
 MQTT_SERVER = "test.mosquitto.org"
 MQTT_PATH = "ef37917e08872c2f2a16d233ec4925ce"
+
+
+camera = PiCamera()
+camera.resolution = (400,400)
+#camera.start_preview()
+sleep(1)
+camera.capture('index.png')
+sleep(1)
 
 f = open('index.png','rb')
 fileContent = f.read()
